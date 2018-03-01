3a54a9beaf35b0c7a7f6e48224d384df84e9ecea
Yash <ymahajan98@gmail.com>
2018-03-01 21:59:15 +0530

    removal

--- hello.c
+++ hello.c
@@ -1,10 +1,5 @@
 #include<stdio.h>
-void microkernel_sendmsg(char *);
 void main(){
 printf("Helloworld!\n");
 printf("This must be a monolithic design\n");
-microkernel_sendmsg("is more portable");
-}
-void microkernel_sendmsg(char *a){
-printf("microkernel: %s\n", a);
 }
