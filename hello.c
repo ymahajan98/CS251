25e0b2482f31aafd6165149a9a5dbece461cbe96
Yash <ymahajan98@gmail.com>
2018-03-01 21:49:38 +0530

    second

--- hello.c
+++ hello.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+void microkernel_sendmsg(char *);
 void main(){
 printf("Helloworld!\n");
+microkernel_sendmsg("is more portable");
+}
+void microkernel_sendmsg(char *a){
+printf("microkernel: %s\n", a);
 }
